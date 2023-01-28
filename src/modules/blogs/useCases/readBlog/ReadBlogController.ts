c9e92c26796f1061f5ff75d6c9e5d0c18eb44cb2
Fernando Andrade <fernandofreamunde@gmail.com>
2023-01-28 18:45:05 +0100

    :bug: fix imports and rename vars

--- src/modules/blogs/useCases/readBlog/ReadBlogController.ts
+++ src/modules/blogs/useCases/readBlog/ReadBlogController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
-import { BlogRepository } from "modules/blogs/infrastructure/BlogRepository";
+import { BlogRepository } from "../../../../modules/blogs/infrastructure/BlogRepository";
 import { ReadBlogUseCase } from "./ReadBlogUseCase";
 
 class ReadBlogController {
   async handle(request: Request, response: Response): Promise<Response<any, Record<string, any>>> {
 
+    const { id } = request.params;
     // replace this with tsrynge container to get UseCase instance
     const repo = new BlogRepository;
     const usecase = new ReadBlogUseCase(repo);
-    const user = await usecase.execute('blogid');
+    const blog = await usecase.execute(id);
 
-    return response.status(200).json({message: 'template'});
+    return response.status(200).json(blog);
   }
 }
 

7f98214adaf0c798d3a99f5dcf193de55dddd895
Fernando Andrade <fernandofreamunde@gmail.com>
2023-02-20 12:00:14 +0100

    🧱 add basic infrastructure

---
+++ src/modules/cards/factory/CardFactory.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+import { Card } from "@prisma/client";
+import { ICardRepository } from "../contracts/ICardRepository";
+
+interface ICardSeed {
+  name?: string;
+  description?: string;
+  type?: string;
+  data?: object;
+  blog_id?: string;
+}
+
+interface IMakeCard {
+  objectData: ICardSeed;
+  repo: ICardRepository;
+}
+
+export default async function makeCard({ objectData, repo }: IMakeCard): Promise<Card> {
+  const {
+    name,
+    description,
+    type,
+    data,
+    blog_id
+  } = objectData;
+
+  const card = await repo.create({
+    description: description ?? faker.lorem.sentence(),
+    name: name ?? faker.company.name(),
+    type: type ?? faker.lorem.word(),
+    data: data ?? { message: faker.lorem.sentence() },
+    blog_id: blog_id ?? `fake-blog-uuid-${faker.random.numeric(4)}`
+  }) as Card;
+
+  return card;
+}
---
+++ src/modules/cards/infrastructure/CardRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const cardRouter = Router();
+
+// const readCardsController = new Controller();
+// cardRouter.post('/cards', Auth, readCardsController.handle);
+
+export { cardRouter };

e09add0fcd4347f8a3a34ec9eee3b782fae79063
Fernando Andrade <fernandofreamunde@gmail.com>
2023-02-21 23:56:13 +0100

    ✅👔 add update card logic

--- src/modules/cards/infrastructure/CardRouter.ts
+++ src/modules/cards/infrastructure/CardRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateCardController } from "../useCases/createCard/CreateCardController";
 import { Auth } from "../../../core/middleware/AuthMiddleware";
+import { UpdateCardController } from "../useCases/updateCard/UpdateCardController";
 
 const cardRouter = Router();
 
 const createCardController = new CreateCardController();
+const updateCardController = new UpdateCardController();
+
 cardRouter.post('/cards', Auth, createCardController.handle);
+cardRouter.patch('/cards/:id', Auth, updateCardController.handle);
 
 export { cardRouter };
